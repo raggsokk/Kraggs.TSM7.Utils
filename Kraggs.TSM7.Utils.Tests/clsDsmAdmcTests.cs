4c3ddb36f73c4b4b72ac89c1c91dae716933172b
Jarle Hansen <jarle.hansen@gmail.com>
2014-01-16 23:54:48 +0100

    Initial Commit of Project Structure.

---
+++ Kraggs.TSM7.Utils.Tests/clsDsmAdmcTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+using Kraggs.TSM7.Utils;
+
+namespace Kraggs.TSM7.Utils.Tests
+{
+    public class clsDsmAdmcTests
+    {
+
+    }
+}
